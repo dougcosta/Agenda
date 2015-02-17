4ee46eae41328985befa29faf50b485c32252599
douglas costa <dgenius@gmail.com>
2015-02-17 14:57:44 -0200

    commit inicial

---
+++ Agenda.Entidades/Compromisso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Entidades
+{
+    public class Compromisso
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+        
+        [Required]
+        public DateTime DataFinal { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Timestamp]
+        public byte[] Versao { get; set; }
+    }
+}
---
+++ Agenda.Repositorios/RepositorioDeCompromissos.cs
@@ -0,0 +1,15 @@
+﻿using Agenda.Dados;
+using Agenda.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Repositorios
+{
+    public class RepositorioDeCompromissos
+    {
+        
+    }
+}
